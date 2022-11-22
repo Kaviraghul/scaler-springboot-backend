3a47c2e9cf4308920db8b32bfe4b022e1604f3a9
Kaviraghul <vettrithamil@gmail.com>
2022-11-22 12:15:18 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/SaclerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaclerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaclerApplication.class, args);
+	}
+
+}
